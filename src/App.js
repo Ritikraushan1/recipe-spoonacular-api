4abcc939580bedd8527c9b8167493913908776a8
Ritikraushan1 <94674369+Ritikraushan1@users.noreply.github.com>
2023-08-20 23:48:36 +0530

    v234

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={"/"}>delicious</Logo>
+      </Nav>
+      {!process.env.REACT_APP_FOOD_API_KEY ? (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please get the API key from{" "}
+          <strong>
+            <a href="https://spoonacular.com/food-api/">Spoonacular Food Api</a>
+          </strong>
+          <br />
+          <br />
+          and add it in your env file with{" "}
+          <strong>"REACT_APP_FOOD_API_KEY"</strong> name and restart the app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        <>
+          <Search />
+          <Category />
+          <Pages />
+        </>
+      )}
     </div>
   );
-}
+};
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
 
 export default App;
